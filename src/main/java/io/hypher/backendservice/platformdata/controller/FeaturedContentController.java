e737262e82d696a9a55fd60f0e7036a113e4391c
zimtz <zer0d1ve@tuta.io>
2024-03-04 10:00:36 +0000

    adding CORS to endpoint

--- src/main/java/io/hypher/backendservice/platformdata/controller/FeaturedContentController.java
+++ src/main/java/io/hypher/backendservice/platformdata/controller/FeaturedContentController.java
@@ -20,6 +20,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,8 +31,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
-
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class FeaturedContentController {
