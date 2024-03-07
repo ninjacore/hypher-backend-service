55f926efc281e32b9cf0cd558e6f36433db9c450
zimtz <zer0d1ve@tuta.io>
2024-03-07 16:48:00 +0000

    code cleanup

--- src/main/java/io/hypher/backendservice/platformdata/dto/TagsUpdate.java
+++ src/main/java/io/hypher/backendservice/platformdata/dto/TagsUpdate.java
@@ -1,10 +1,8 @@
 package io.hypher.backendservice.platformdata.dto;
 
-import java.util.ArrayList;
 
 public class TagsUpdate {
 
-    // ArrayList<String> tags;
     String tags;
 
     public TagsUpdate() {
@@ -18,17 +16,4 @@ public class TagsUpdate {
         this.tags = tags;
     }
 
-    // public ArrayList<String> getTags() {
-    //     return tags;
-    // }
-
-    // public void setTags(ArrayList<String> tags) {
-    //     this.tags = tags;
-    // }
-
-    
-
-    
-    
-    
 }
