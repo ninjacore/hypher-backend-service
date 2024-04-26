c7a180eac4de529d15851dcf4b56b9e5437ab57b
zimtz <zer0d1ve@tuta.io>
2024-04-26 11:47:16 +0100

    alternative ports

--- src/main/java/io/hypher/backendservice/platformdata/controller/LinkCollectionController.java
+++ src/main/java/io/hypher/backendservice/platformdata/controller/LinkCollectionController.java
@@ -38,8 +38,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
-
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
+// @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class LinkCollectionController {
